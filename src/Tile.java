027cb8e73420b7e2f5ed7bc9df8b0c1e13f3c155
chris <chriskjellqvis@gmail.com>
2016-04-23 20:45:57 -0500

    Holds state and value for the tile on the board.
    State determines whether it's blank or if it is a special value tile or if it has a letter on it.
    
    Value holds the letter.

---
+++ src/Tile.java
@@ -0,0 +1,29 @@
+/**
+ * Created by chris on 4/23/16.
+ */
+public class Tile {
+    /**
+     * State determines what is showing on the board.
+     *
+     * 0 blank
+     * 1 star
+     * 2 double letter
+     * 3 triple letter
+     * 4 double word
+     * 5 triple word
+     * 6 placed tile
+     *
+     */
+    public int state;
+
+    /**
+     * Only relevant of value of tile is 6.
+     */
+    public char letter;
+
+    public Tile(int state){
+        this.state = state;
+    }
+
+
+}
