bb422a7e46cdf0f921561c8f006acb06260608db
chris <chriskjellqvis@gmail.com>
2016-04-23 20:47:33 -0500

    Opens up the default frame and has no game functionality as of yet.

--- src/main.java
+++ src/main.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
 /**
  * Created by chris on 4/23/16.
  */
 public class main {
+    public static void main(String[] args) throws IOException{
+        JFrame frame = new JFrame("frame");
+        frame.setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
+        frame.add(new Display());
+        frame.pack();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
